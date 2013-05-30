1cf8d0b9e092b36d9d86c7643466d15958df8c19
Peter Odding <peter@peterodding.com>
2013-05-30 14:34:23 +0200

    Bug fix for setup.py script

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 from os.path import abspath, dirname, join
-from setuptools import setup
+from setuptools import setup, find_packages
 
 # Fill in the long description (for the benefit of PyPi)
 # with the contents of README.rst (rendered by GitHub).
@@ -9,13 +9,13 @@ readme_file = join(dirname(abspath(__file__)), 'README.rst')
 readme_text = open(readme_file, 'r').read()
 
 setup(name='pip-accel',
-      version='0.8.6',
+      version='0.8.7',
       description='Accelerator for pip, the Python package manager',
       long_description=readme_text,
       author='Peter Odding',
       author_email='peter.odding@paylogic.eu',
       url='https://github.com/paylogic/pip-accel',
-      py_modules=['pip_accel'],
+      packages=find_packages(),
       entry_points={'console_scripts': ['pip-accel = pip_accel:main']},
       install_requires=['pip >= 1.3', 'coloredlogs'],
       tests_require=['virtualenv'],
