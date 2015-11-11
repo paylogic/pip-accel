fefc488a783a9d30f5d2a100af1dba74542b35a2
Peter Odding <peter@peterodding.com>
2015-11-11 01:28:52 +0100

    Argh let's just get this over with :-(
    https://ci.appveyor.com/project/xolox/pip-accel/build/1.0.90

--- scripts/appveyor.py
+++ scripts/appveyor.py
@@ -13,7 +13,7 @@ import os
 from humanfriendly import concatenate
 
 # Test dependencies.
-from executor import execute, get_search_path, which
+from executor import ExternalCommandFailed, execute, get_search_path, which
 
 print("FakeS3 executables:\n%r" % which('fakes3'))
 
@@ -25,7 +25,11 @@ for program in which('fakes3'):
         padding = vertical_whitespace + delimiter + vertical_whitespace
         print(padding + ("%s:" % program) + padding + contents + padding)
 
-execute('fakes3', '--help')
+for program in ['fakes3', 'fakes3.bat'] + which('fakes3'):
+    try:
+        execute(program, '--help')
+    except ExternalCommandFailed:
+        print "%s doesn't work?!" % program
 
 print("Executable search path:\n\n%s" % "\n\n".join(
     "%s:\n%s" % (d, concatenate(sorted(os.listdir(d))))
